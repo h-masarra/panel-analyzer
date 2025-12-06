e7716b9c6ce69bddeb507fda925ad556350b9414
h-masarra <h.massarra@melqar.com>
2025-12-06 22:08:05 +0200

    Update __init__.py

--- api/analyze/__init__.py
+++ api/analyze/__init__.py
@@ -1,1 +1,8 @@
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        '{"message": "Analyzer API is working!"}',
+        mimetype="application/json"
+    )
 
